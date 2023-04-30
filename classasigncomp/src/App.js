0bad362c34f33769d9d7c990f64c0e22cf402d65
debashish sahu <debashish7862@gmail.com>
2023-04-30 16:33:05 +0530

    netflixclasscomp

---
+++ classasigncomp/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Nav from './Component/Nav';
+import Body from './Component/Body';
+
+function App() {
+  return (
+    <div>
+     <Nav/>
+     <Body/>
+    </div>
+  );
+}
+
+export default App;
---
+++ classasigncomp/src/Component/Body.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import './body.css'
+export default class Body extends Component {
+  render() {
+    return (
+      <div className='bodyStyle'>
+
+      </div>
+    )
+  }
+}
